e7f84d2b5018560f9a5680de36e881b003154c6f
lukkanog <lukanog@hotmail.com>
2021-01-20 22:07:58 -0300

    comandos finalizados

--- teste-sql-wipro.sql
+++ teste-sql-wipro.sql
@@ -72,7 +72,7 @@ ON P.idStatus = S.idStatus
 
 -- Comando 2:
 SELECT MAX(A.dtANdamento)
---AS 'Maior data de andamento com processos encerrados em 2013'
+AS 'Maior data de andamento com processos encerrados em 2013'
 FROM tb_Andamento A
 INNER JOIN tb_Processo P
 ON P.idProcesso = A.idProcesso
@@ -87,6 +87,6 @@ HAVING COUNT(DtEncerramento) > 5
 
 -- Comando 4:
 SELECT RIGHT('000000000000' + CAST(nroProcesso as VARCHAR(12)), 12) 
---AS 'Números de identificação dos processos'
+AS 'Números de identificação dos processos'	
 FROM tb_Processo
 
